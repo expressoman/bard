b48594cbdf521d09353653181fbc4ccab6115b56
Luke Taylor <luke.taylor@guardian.co.uk>
2016-06-17 11:44:28 +0100

    Add spinner when loading page insights.
        This is needed as the initial request for a specfic page with its from and to parameters will hit the
        FB api directly and not benefit from server side caching. Therefore this shows the user that something is actually happening.

--- public/components/pageInsightsController.react.js
+++ public/components/pageInsightsController.react.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Page from './page.react';
 import PageInsightsService from '../services/PageInsightsService';
+import Loader from 'react-loader';
 
 export default class PageInsightsController extends React.Component {
 
@@ -8,6 +9,7 @@ export default class PageInsightsController extends React.Component {
         super(props);
 
         this.state = {
+            loaded: false,
             pageInsightsData: undefined
         }
     }
@@ -23,16 +25,23 @@ export default class PageInsightsController extends React.Component {
     }
 
     loadPageInsights(page, from, to) {
+        /* Means spinner will show when changing between page insight pages. */
+        this.setState({loaded: false, pageInsightsData: this.state.pageInsightsData});
         PageInsightsService.getPageInsights(page, from, to).then( response => {
-            this.setState({pageInsightsData: response});
+            this.setState({loaded: true, pageInsightsData: response});
         });
     }
 
     render() {
-        if (!this.state.pageInsightsData) { return false };
-
         return (
-            <Page data={this.state.pageInsightsData} />
+            <Loader loaded={this.state.loaded} lines={13} length={20} width={10} radius={30}
+                corners={1} rotate={0} direction={1} color="#000" speed={1}
+                trail={60} shadow={false} hwaccel={false} className="spinner"
+                zIndex={2e9} top="50%" left="50%" scale={1.00}
+                loadedClassName="loadedContent" >
+
+                <Page data={this.state.pageInsightsData} />
+            </Loader>
         );
     }
 }
